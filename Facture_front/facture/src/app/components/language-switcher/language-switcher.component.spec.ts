ae251e8b2faae2ed106323eaa0c832f32e75effc
alachebil <ala.chebil@esprit.tn>
2025-05-20 12:06:22 +0100

    change

--- Facture_front/facture/src/app/components/language-switcher/language-switcher.component.spec.ts
+++ Facture_front/facture/src/app/components/language-switcher/language-switcher.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LanguageSwitcherComponent } from './language-switcher.component';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { of } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+
+// Mock loader qui retourne un Observable vide (aucune traduction)
+class FakeLoader implements TranslateLoader {
+  getTranslation(lang: string) {
+    return of({}); // retourne un objet vide pour les traductions
+  }
+}
 
 describe('LanguageSwitcherComponent', () => {
   let component: LanguageSwitcherComponent;
@@ -8,10 +18,17 @@ describe('LanguageSwitcherComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LanguageSwitcherComponent]
-    })
-    .compileComponents();
+      declarations: [LanguageSwitcherComponent],
+      imports: [
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useClass: FakeLoader }
+        }),
+        MatIconModule
+      ]
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(LanguageSwitcherComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();

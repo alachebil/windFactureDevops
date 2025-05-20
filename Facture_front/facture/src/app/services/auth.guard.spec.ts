ee2bc3cc7cc8ee22d5569fa794aaf45602b548d3
alachebil <ala.chebil@esprit.tn>
2025-05-20 11:33:41 +0100

    changement du file content

--- Facture_front/facture/src/app/services/auth.guard.spec.ts
+++ Facture_front/facture/src/app/services/auth.guard.spec.ts
@@ -1,17 +1,59 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthService } from './AuthService';
+import { AuthGuard } from './auth.guard';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
-import { authGuard } from './auth.guard';
-
-describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+describe('AuthGuard', () => {
+  let guard: AuthGuard;
+  let authServiceMock: any;
+  let routerMock: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    authServiceMock = {
+      isAuthenticated: jasmine.createSpy('isAuthenticated')
+    };
+
+    routerMock = {
+      navigate: jasmine.createSpy('navigate')
+    };
+
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [
+        AuthGuard,
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: Router, useValue: routerMock }
+      ]
+    });
+
+    guard = TestBed.inject(AuthGuard);
   });
 
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+  function createMockState(url: string): RouterStateSnapshot {
+    return { url } as RouterStateSnapshot;
+  }
+
+  it('should allow access when authenticated and route is allowed', () => {
+    authServiceMock.isAuthenticated.and.returnValue(true);
+    localStorage.setItem('role', 'System');
+    const result = guard.canActivate({} as ActivatedRouteSnapshot, createMockState('/system'));
+    expect(result).toBeTrue();
+  });
+
+  it('should redirect to 404 when role is PARTNER accessing /system', () => {
+    authServiceMock.isAuthenticated.and.returnValue(true);
+    localStorage.setItem('role', 'PARTNER');
+    const result = guard.canActivate({} as ActivatedRouteSnapshot, createMockState('/system'));
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/404']);
+    expect(result).toBeTrue(); // toujours true même si redirection, selon ton code
+  });
+
+  it('should deny access when not authenticated', () => {
+    authServiceMock.isAuthenticated.and.returnValue(false);
+    const result = guard.canActivate({} as ActivatedRouteSnapshot, createMockState('/home'));
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/404']);
+    expect(result).toBeFalse();
   });
 });
